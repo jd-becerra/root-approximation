a9b95ac9e8b0680599015f28c94d3fa099daf94e
jd-becerra <jd-becerra>
2023-04-25 12:32:56 -0600

    added method Newton-Raphson

---
+++ newtonRaphson/newton.js
@@ -0,0 +1,194 @@
+/** METODO DE BISECCIÓN*/
+
+window.onload = () => {
+
+  //Eliminar los diálogos de Inicio y de Error cuando el usuario haga click en la página
+  document.addEventListener('mouseup', function(e) {
+    document.getElementById("initWarning").style.display = "none";
+    if(document.getElementById("errorWarning").style.display == "block"){
+      document.getElementById("errorWarning").style.display = "none";
+    }
+  });
+
+  //resetValues(); 
+
+  //Mostrar el critero de paro elegido
+  let stopMethods = document.getElementById("stopMethods");
+  stopMethods.value = '0';
+  stopMethods.addEventListener('change', function(e){
+    switch(this.value) {
+      case '0':
+        document.getElementById("iteration").value = 0;
+        document.getElementById("iterationMethod").style.display = "none";
+        document.getElementById("criteriaMethod").style.display = "block";
+        break;
+      case '1':
+        document.getElementById("criteria").value = 0;
+        document.getElementById("criteriaMethod").style.display = "none";
+        document.getElementById("iterationMethod").style.display = "block";
+        break;
+      case '2':
+        document.getElementById("iterationMethod").style.display = "none";
+        document.getElementById("criteriaMethod").style.display = "none";
+        break;
+    }
+  });
+
+  //Tomar valores si se presiona el botón de "resultado"
+  document.getElementById('result').addEventListener('click', function(e) {
+    let xL = document.getElementById('xL').value;
+    let xU = document.getElementById('xU').value;
+    let squareVal = document.getElementById('xSqr').value;
+    let linVal = document.getElementById('xLnl').value;
+    let independentVal = document.getElementById('indVal').value;
+    let criteriaOption = document.getElementById('stopMethods').value;
+
+    //Validar que los campos no estén vacíos ni incorrectos
+    limits = [xL, xU];
+    values = [squareVal, linVal, independentVal];
+    let xI = 0;
+    let setLimit = false;
+    if (checkValues(limits,values, +criteriaOption)) {
+      if(!isNotNumberValid(xL) && !isNotNumberValid(xU)) {
+        let xI = (xL + xU) / 2;
+      } else {
+        if(!isNotNumberValid(xL)) {
+          let xI = xL;
+        } else {
+          let xI = xU;
+        } 
+      }
+
+      document.getElementById('iterationTable').style.border = "1px solid white";
+      document.getElementById('iterationTable').innerHTML = `<tr><th>x</th><th>f(x)</th></tr>`;
+      let calculator = new NewtonRaphson();
+      let res = calculator.newtonRaphson(+xI, +squareVal, +linVal, +independentVal, +criteriaOption);
+      document.getElementById("root").value = res;
+    } else {
+      document.getElementById('errorWarning').style.display = "block";
+    }
+  });
+
+  //Resetear valores a 0
+  document.getElementById('reset').addEventListener('click', function(e) {
+    resetValues();
+  });
+}
+
+function resetValues() {
+  document.getElementById('xL').value = 0;
+  document.getElementById('xU').value = 0;
+  document.getElementById('xSqr').value = 0;
+  document.getElementById('xLnl').value = 0;
+  document.getElementById('indVal').value = 0;
+  document.getElementById('criteria').value = 0;
+  document.getElementById('iteration').value = 0;
+  document.getElementById('root').value = 0;
+  document.getElementById('iterationTable').innerHTML = ""; 
+  document.getElementById('iterationTable').style.border = "none"; 
+}
+
+function checkValues(limits, values, criteriaOption) {
+  if (isNotNumberValid(limits[0]) && isNotNumberValid(limits[1])) {
+    return false;
+  }
+  for (let val of values) {
+    if (isNotNumberValid(val)) {
+      return false;
+    }
+  }
+  if (values[0] == values[1]) {
+    return false; //if xL equal to xU
+  }
+  //check values for stopping iteration
+  if (criteriaOption == 0){
+    let percentage = document.getElementById('criteria').value;
+    if (isNotNumberValid(percentage) || +percentage <= 0) {
+      return false;
+    }
+  }
+  if(criteriaOption == 1) {
+    let iterations = document.getElementById('iteration').value;
+    if (isNotNumberValid(iterations) || +iterations <= 0 || !(iterations%1==0)) {
+      return false;
+    }
+  }
+  return true; //only if all values are valid
+}
+
+function isNotNumberValid(val) {
+  if (val == "0") {
+    return false;
+  }
+  return (isNaN(val) && isNaN(parseFloat(val))) || val.trim() == "" || !val;
+}
+
+//Clase con el método para calcular el resultado  (Oiga compañero Aldo que es esto xd)
+class NewtonRaphson{
+  newtonRaphson(xI, squareVal, linVal, independentVal, criteriaOption) {
+    let fXI = this.functionX(xI,squareVal,linVal,independentVal);
+    let dXI = this.derivativeX(xI, squareVal, linVal, independentVal);
+
+    //Calcular primera X
+    let xR = xI - (fXI / dXI);
+    let funcXR = this.functionX(xR,squareVal,linVal, independentVal);
+    
+    this.updateIterationTable(xR, funcXR);
+
+    let criteria = 0;
+
+    //Iniciar iteraciones
+    do {
+      console.log(xI, xR);
+      xI = xR;
+      fXI = this.functionX(xI,squareVal,linVal,independentVal);
+      dXI = this.derivativeX(xI,squareVal,linVal);
+      xR = xI - (fXI / dXI);
+      funcXR = this.functionX(xR,squareVal,linVal,independentVal);
+
+      this.updateIterationTable(xR, funcXR);
+
+      //Utilizar un metodo para evaluar si parar o no
+      if(criteriaOption == 0 || criteriaOption == 2) {
+        criteria = this.relativeError(xR, xI);
+      } else if(criteriaOption == 1) {
+        criteria++;
+      }
+
+    } while(this.evaluateStop(criteria, criteriaOption));
+      
+    return xR;
+  }
+
+  evaluateStop(criteria, option) {
+    switch(option) {
+      case 0:
+        return criteria > +(document.getElementById('criteria').value);
+        break;
+      case 1:
+        return criteria < (+(document.getElementById('iteration').value) - 1);
+        break;
+      case 2:
+        return criteria >= 0.001;
+        break;
+    }
+  }
+
+  functionX(x, squareVal, linVal, independentVal) {
+    return (squareVal*(x*x))+(linVal*x)+independentVal;
+  }
+
+  derivativeX(x, squareVal, linVal) {
+    return (squareVal*2*x)+linVal;
+  }
+
+  relativeError(newVal, previousVal) {
+    let error = (((newVal - previousVal)/newVal)*100);
+    return (error >= 0) ? error : (error * -1);
+  }
+
+  updateIterationTable(x, fx) {
+    let table = document.getElementById('iterationTable');
+    table.innerHTML += `<tr><td>${x}</td><td>${fx}</td></tr>`;
+  }
+}
